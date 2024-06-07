42b51bd247b3703f6d4b39a062c3b68de5fd85b3
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2024-06-07 13:37:48 +0100

    Corrected my views

--- sports_store/store/api/serializers.py
+++ sports_store/store/api/serializers.py
@@ -1,14 +1,27 @@
 # within app imports
-from store.models import Category
+from store.models import Item, Brand, Category
 
 # rest framework imports
 from rest_framework import serializers
 
+class BrandSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Brand
+        fields = '__all__'
+        
 
-class ItemSerializer(serializers.ModelSerializer):
-    brand = serializers.StringRelatedField()
-    category = serializers.StringRelatedField()
+class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Category
+        fields = '__all__'
+        
+
+class ItemSerializer(serializers.ModelSerializer):
+    brand = serializers.PrimaryKeyRelatedField(queryset=Brand.objects.all())
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    
+    class Meta:
+        model = Item
         fields = '__all__'
